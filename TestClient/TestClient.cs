bd1aeb64338dedfcf04f932f02194265f64dbfce
lepesa <lepesa@users.noreply.github.com>
2015-11-02 20:49:53 +0200

    Lisenssi päivitetty tiedostoihin.

--- TestClient/TestClient.cs
+++ TestClient/TestClient.cs
@@ -1,7 +1,18 @@
-﻿using System;
+﻿/*
+   Copyright 2015 Esa Leppänen
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
+using System;
 using MNISTTester;
-
 
 namespace TestClient
 {
@@ -12,7 +23,7 @@ namespace TestClient
     public class TestClient : IMNISTTest
     {
 
-        private const string VERSION_STRING = "TestClient example v1.0";
+        private const string VERSION_STRING = "TestClient example v1.1";
 
         private Network network;
         private int materialIndex;
